8d9e3cdd364bc6886e3213fa6c6cf492723b441c
Stefan Fuchs <stefan1234@gmail.com>
2023-02-14 23:29:22 -0300

    implement accounts endpoint

---
+++ docs/curl.md
@@ -0,0 +1,8 @@
+curl -i \
+  -d '{"account_number":"A"}' \
+  -X POST \
+  -H "Content-Type: application/json" \
+  http://localhost:3000/accounts
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 
 
 import express from 'express';
-import BetterSqlite3 from 'better-sqlite3';
+import BetterSqlite3, { SqliteError } from 'better-sqlite3';
+import { createId } from '@paralleldrive/cuid2';
+import { Account } from './types';
 
 const app = express();
 const db = new BetterSqlite3('data.db');
@@ -12,19 +14,48 @@ const db = new BetterSqlite3('data.db');
 
 const createTable = db.prepare(`
 CREATE TABLE IF NOT EXISTS account (
-  id VARCHAR(30) PRIMARY KEY DEFAULT (uuid()),
+  id VARCHAR(30) PRIMARY KEY NOT NULL,
   balance INTEGER NOT NULL DEFAULT (0)
 );`);
 
 createTable.run();
 
+
+const printAllAccounts = () => {
+  const selectData = db.prepare(`SELECT * FROM account;`);
+  const res = selectData.all();
+  console.log(res)
+}
+
+
 app.use(express.json());
 
-app.post('/data', (req, res) => {
-  const insertData = db.prepare(`INSERT INTO data (data) VALUES (?);`);
-  insertData.run(req.body.data);
+app.post('/accounts', (req, res) => {
 
-  res.status(201).send({ message: 'Data added successfully' });
+  const accountNumber = req.body.account_number || createId();
+  const insertData = db.prepare(`INSERT INTO account (id) VALUES (?);`);
+
+  try {
+    insertData.run(accountNumber);
+  } catch (e: any) {
+    const error = e as SqliteError
+
+    if (e.code === 'SQLITE_CONSTRAINT_PRIMARYKEY') {
+      // duplicate account id was specified
+      res.status(409).send();
+      return
+    }
+
+    console.log(e)
+    res.status(400).send();
+    return;
+  }
+
+  printAllAccounts()
+
+  // console.log()
+
+  res.status(201).send({ account_number: accountNumber });
 });
 
 const port = process.env.PORT || 3000;
